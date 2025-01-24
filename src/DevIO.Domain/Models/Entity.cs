ca698124922deaa4cc95c1c8dcd49b44ecb65236
ezequiel-lima <ezequiel.lima.br@gmail.com>
2025-01-24 15:42:40 -0300

    Adicionar arquivos de projeto.

---
+++ src/DevIO.Domain/Models/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace DevIO.Domain.Models
+{
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
